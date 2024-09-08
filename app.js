d5a963ff64d1f3dec487b75bdeb2a2e8134cfe92
James <rotichjames8@gmail.com>
2024-09-08 23:42:29 +0300

    node practice

--- app.js
+++ app.js
@@ -26,6 +26,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1
+  const tour = tours.find(el => el.id === id) 
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status:"fail",
+      message: "invalid ID"
+    })
+  }
+
+  
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post("/api/v1/tours", (req, res) => {
  const newId = tours[tours.length-1].id + 1
  const newTour = Object.assign({id:newId}, req.body)
@@ -39,6 +60,7 @@ app.post("/api/v1/tours", (req, res) => {
         message: "Internal Server Error: Could not save tour.",
       });
     }}
+    
 res.status(201).json({
   status: "success",
   data: {
@@ -49,6 +71,35 @@ res.status(201).json({
 
 })
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    })
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: `updated tour`
+    }
+  })
+})
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID"
+    })
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null
+  })
+})
 app.listen(3000, () => {
   console.log('listening...');
 });
