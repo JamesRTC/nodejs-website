c617eb699c74bde6a2847a780d5b03b78f23e30a
James <rotichjames8@gmail.com>
2024-11-10 23:22:24 +0300

    node practice

--- app.js
+++ app.js
@@ -15,4 +15,27 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (res, req, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `can't find ${req.originalURL} on this server!`,
+  // });
+
+  const err = new Error(`can't find ${req.originalURL} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status || 'error',
+    message: err.message,
+  });
+});
+
 module.exports = app;
