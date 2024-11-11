86c58c6fe8fb94c91bb29b6e7d7d0c2d452c57d9
James <rotichjames8@gmail.com>
2024-11-11 23:43:12 +0300

    node practice

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,26 +18,13 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (res, req, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `can't find ${req.originalURL} on this server!`,
-  // });
+  // const err = new Error(`can't find ${req.originalURL} on this server!`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
 
-  const err = new Error(`can't find ${req.originalURL} on this server!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-
-  next(err);
+  next(new AppError(`can't find ${req.originalURL} on this server!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status || 'error',
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
